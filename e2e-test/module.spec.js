a68b5cd1b0a5db52267cf50951b5856ad33d42c0
Martin Probst <martin@probst.io>
2014-10-07 16:24:01 -0700

    feat(tests): Remove integration tests.
    e2e-tests now live in the common karma integration test repository at
    https://github.com/karma-runner/integration-tests/tree/master/googmodule

--- e2e-test/module.spec.js
+++
@@ -1,9 +0,0 @@
-goog.module('my.test');
-
-var mod = goog.require('my.module');
-
-describe('goog.module based tests', function() {
-  it('should load modules', function() {
-    expect(mod.greet()).toBe('hello world');
-  });
-});
